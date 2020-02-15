c7dc1fec04134c36801d4347d9e050c2922f492a
Winzmcman <nikithobbi@gmail.com>
2020-02-15 18:11:26 +0200

    test1.1

--- nikita_answer_bot.php
+++ nikita_answer_bot.php
@@ -2,11 +2,10 @@
 
 $token = "871852857:AAG5BPMscXpTAO3zMGZX6xzY_fy65a52GIk";
 
-$comand = "SendMessage";
+$post = json_decode($_POST['Update'])['message'];
+$comand = "sendMessage";
 
-$post = $_POST['message'];
-
-$answer = file_get_contents("https://api.telegram.org/bot$token/$comand?chat_id=871852857&text=$post");
+$answer = file_get_contents("https://api.telegram.org/bot$token/$comand?chat_id=850034955&text=$post");
 
 $data = json_decode($answer);
 
