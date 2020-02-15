025bd2e61deabc2175c5162421312769c80bb1fc
Winzmcman <nikithobbi@gmail.com>
2020-02-15 17:27:43 +0200

    test3

--- nikita_answer_bot.php
+++ nikita_answer_bot.php
@@ -2,7 +2,7 @@
 
 $token = "871852857:AAG5BPMscXpTAO3zMGZX6xzY_fy65a52GIk";
 
-$comand = "getUpdates";
+$comand = "SendMessage";
 
 $answer = file_get_contents("https://api.telegram.org/bot$token/$comand?chat_id=850034955&text=hi pituh");
 
